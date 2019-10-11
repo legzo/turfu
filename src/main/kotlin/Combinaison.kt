cd39bd0f097db9491e4a2c43ab982e6239bb9d6b
Julien Terrier <julien.terrier@orange.com>
2019-10-11 14:48:38 +0200

    Supprime une ligne inutile

--- src/main/kotlin/Combinaison.kt
+++ src/main/kotlin/Combinaison.kt
@@ -1,7 +1,5 @@
 inline class Combinaison(val value: Set<Int>)
 
-fun allFrom(vararg combinaisons: List<Combinaison>) = combinaisons.toList().flatten()
-
 fun List<Combinaison>.limitOccurencesTo(times: Int) =
     this.groupBy { it }
         .map { (combinaison, combinaisons) -> combinaison to combinaisons.count() }
